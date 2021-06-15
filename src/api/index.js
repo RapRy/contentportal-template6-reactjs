30fe8d5a2369b0911974cf4946df32c4a6c2cb81
Ryan <ry.quickcell@gmail.com>
2021-06-15 14:35:20 +0800

    fixed issue

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = "https://port-template-1.herokuapp.com/template9"
+const url = "https://port-template-1.herokuapp.com"
 
 export const fetchCategories = (cat) => axios.get(`${url}/template9/categories/${cat}`)
 export const fetchContents = (cat, subcat) => axios.get(`${url}/template9/contents/${cat}/${subcat}`)
